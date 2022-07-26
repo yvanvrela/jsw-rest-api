58651c7d4530fd0912161a1aa8cf41915283b5b0
Yvan <varelaivan30@gmail.com>
2022-07-26 11:44:12 -0400

    Added register routes

--- src/application.py
+++ src/application.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
-from extensions import config_extension
+from extensions import config_extension, routes_extensions
 
 
 def create_app():
     app = Flask(__name__)
 
     config_extension.register_config(app)
+    routes_extensions.register_routes(app)
 
     return app
