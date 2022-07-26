5f46eef38c3811de703ab0a79e76faeeeb366096
Yvan <varelaivan30@gmail.com>
2022-07-25 20:29:38 -0400

    Resolved app.config

--- src/extensions/config_extension.py
+++ src/extensions/config_extension.py
@@ -1,5 +1,7 @@
+from flask import Flask
 from config import configurations
 
 
-def register_config(app):
-    pass
+def register_config(app: Flask):
+
+    app.config.from_object(configurations['development'])
