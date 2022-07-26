fe52a76a713466a2d32670d9c1a67ea830d2cc20
Yvan <varelaivan30@gmail.com>
2022-07-26 11:46:35 -0400

    Refactor files

---
+++ src/apps/api/function_jwt.py
@@ -0,0 +1,37 @@
+from jwt import encode, decode
+from jwt import exceptions
+from os import getenv
+from datetime import datetime, timedelta
+from flask import jsonify
+
+
+def expire_date(days: int) -> int:
+    now_date = datetime.now()
+    expire_date = now_date - timedelta(days)
+
+    return expire_date
+
+
+def write_token(data: dict):
+    token = encode(
+        payload={**data, 'exp': expire_date(1)},
+        key=getenv('SECRET_KEY'),
+        algorithm='HS256'
+    )
+
+    return token.encode('UTF-8')
+
+
+def validate_token(token: str, output=False):
+    try:
+        if output:
+            return decode(token, key=getenv('SECRET_KEY'), algorithms=['HS256'])
+        decode(token, key=getenv('SECRET_KEY'), algorithms=['HS256'])
+
+    except exceptions.DecodeError:
+        response = jsonify({'message': 'Invalid Token'}), 401
+        return response
+
+    except exceptions.ExpiredSignatureError:
+        response = jsonify({'message': 'Token Expired'}), 401
+        return response
